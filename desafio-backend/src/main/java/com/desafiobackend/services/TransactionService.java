1678e348d9d9477a2c295cb838b2544c2e5d1e69
IsaqueBeirao <isaquebeirao@gmail.com>
2024-01-16 13:06:14 -0300

    feat: adding class Transaction service

---
+++ desafio-backend/src/main/java/com/desafiobackend/services/TransactionService.java
@@ -0,0 +1,75 @@
+package com.desafiobackend.services;
+
+
+import com.desafiobackend.domain.user.User;
+import com.desafiobackend.domain.user.transaction.Transaction;
+import com.desafiobackend.dtos.TransactionDTO;
+import com.desafiobackend.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Service
+public class TransactionService {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private TransactionRepository repository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private NotificationService notificationService;
+
+    public Transaction createTransaction(TransactionDTO transaction) throws Exception {
+        User sender = this.userService.findUserById(transaction.senderId());
+        User receiver = this.userService.findUserById(transaction.receiverId());
+
+        userService.validateTransaction(sender, transaction.value());
+
+        boolean isAuthorized = this.authorizeTransaction(sender, transaction.value());
+
+        if (!isAuthorized){
+            throw new Exception("unauthorized transaction");
+        }
+
+        Transaction newTransaction = new Transaction();
+        newTransaction.setTransectionValue(transaction.value());
+        newTransaction.setSender(sender);
+        newTransaction.setReceiver(receiver);
+        newTransaction.setTransferTime(LocalDateTime.now());
+
+        sender.setBalance(sender.getBalance().subtract(transaction.value()));
+        receiver.setBalance(receiver.getBalance().add(transaction.value()));
+
+        this.repository.save(newTransaction);
+        this.userService.saveUser(sender);
+        this.userService.saveUser(receiver);
+
+        this.notificationService.sendNotification(sender, "transaction completed successfully");
+
+        this.notificationService.sendNotification(receiver, "transaction received successfully");
+
+
+        return newTransaction;
+    }
+
+    public boolean authorizeTransaction(User sender, BigDecimal value) {
+        ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc", Map.class);
+
+        if (authorizationResponse.getStatusCode() == HttpStatus.OK) {
+            String message = (String) authorizationResponse.getBody().get("message");
+
+            return "Autorizado".equalsIgnoreCase(message);
+        }else  return false;
+    }
+}
