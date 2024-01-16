6f1937c4cbc5d002695c6ffa31a07bea2f774b4e
IsaqueBeirao <isaquebeirao@gmail.com>
2024-01-16 13:08:27 -0300

    feat: adding the transaction controller

---
+++ desafio-backend/src/main/java/com/desafiobackend/controllers/TransactionController.java
@@ -0,0 +1,29 @@
+package com.desafiobackend.controllers;
+
+
+import com.desafiobackend.domain.user.transaction.Transaction;
+import com.desafiobackend.dtos.TransactionDTO;
+import com.desafiobackend.services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transaction")
+public class TransactionController {
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<Transaction> createTransaction (@RequestBody TransactionDTO transaction) throws Exception {
+
+        Transaction newTransaction = this.transactionService.createTransaction(transaction);
+
+        return new ResponseEntity<>(newTransaction, HttpStatus.OK);
+    }
+}
